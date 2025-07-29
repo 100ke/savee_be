d009151b82a1b8e327af37f604ca5c4cdbde02ae
mnxx2 <alsdk4931@gmail.com>
2025-07-30 02:05:26 +0900

    [250730] 수입/지출 내역 CRUD 구현, app.js에 추가

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -71,7 +71,31 @@ const getTransactions = async (userId, ledgerId) => {
   }
 };
 
-const findTransaction = async (userId, ledgerId, transactionId) => {};
+const findTransaction = async (userId, ledgerId, transactionId) => {
+  try {
+    const user = await models.User.findBypk(userId);
+
+    if (!user) {
+      return { message: "로그인이 필요합니다." };
+    }
+
+    const ledger = await models.Ledger.findByPk(ledgerId);
+
+    if (!ledger) {
+      return { message: "가계부를 찾을 수 없습니다." };
+    }
+
+    if (ledger.id == ledgerId && ledger.userId == userId) {
+      const transac = await models.Transaction.findByPk(transactionId);
+
+      return { message: "수입/지출 내역을 가져왔습니다.", data: transac };
+    } else {
+      return { message: "가계부에 접근할 권한이 없습니다." };
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 
 const updateTransaction = async (
   userId,
@@ -81,9 +105,65 @@ const updateTransaction = async (
   type,
   amount,
   memo
-) => {};
+) => {
+  try {
+    const user = await models.User.findByPk(userId);
 
-const deleteTransaction = async (userId, ledgerId, transactionId) => {};
+    if (!user) {
+      return { message: "로그인이 필요합니다." };
+    }
+
+    const ledger = await models.Ledger.findByPk(ledgerid);
+
+    if (!ledger) {
+      return { message: "가계부를 찾을 수 없습니다." };
+    }
+
+    if (ledger.id == ledgerId && ledger.userId == userId) {
+      const transaction = await models.Transaction.findByPk(transactionId);
+
+      if (transaction) {
+        if (categoryId) transaction.categoryId = categoryId;
+        if (type) transaction.type = type;
+        if (amount) transaction.amount = amount;
+        if (memo) transaction.memo = memo;
+      }
+
+      await transaction.save();
+      return { message: "수입/지출 내역을 수정했습니다.", data: transaction };
+    } else {
+      return { message: "가계부에 접근할 권한이 없습니다." };
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteTransaction = async (userId, ledgerId, transactionId) => {
+  try {
+    const user = await models.User.findByPk(userId);
+
+    if (!user) {
+      return { message: "로그인이 필요합니다." };
+    }
+
+    const ledger = await models.Ledger.findByPk(ledgerid);
+
+    if (!ledger) {
+      return { message: "가계부를 찾을 수 없습니다." };
+    }
+
+    if (ledger.id == ledgerId && ledger.userId == userId) {
+      await models.Transaction.destroy(transactionId);
+
+      return { message: "수입/지출 내역이 삭제되었습니다." };
+    } else {
+      return { message: "가계부에 접근할 권한이 없습니다." };
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   addTransactions,
