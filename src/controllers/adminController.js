3c062fbab704abbfb354244fda3258e794665e2f
100ke <bhy9803@gmail.com>
2025-07-31 23:48:34 +0900

    [250731] 관리자 - 회원관리 (CRUD) 구현

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -2,15 +2,8 @@ const adminService = require("../services/adminService");
 
 const createUser = async (req, res) => {
   try {
-    const userId = req.user.id;
     const { email, password, name, role } = req.body;
-    const result = await adminService.createUser(
-      email,
-      name,
-      password,
-      role,
-      userId
-    );
+    const result = await adminService.createUser(email, name, password, role);
     res.status(200).json(result);
   } catch (error) {
     const status = error.status || 500;
@@ -19,4 +12,46 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const getAllUsers = async (req, res) => {
+  try {
+    const result = await adminService.getAllUsers();
+    res.status(200).json(result);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.message || "회원 목록 조회 중 오류가 발생했습니다.";
+    res.status(status).json({ error: message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { name, email, password, role } = req.body;
+    const result = await adminService.updateUser(
+      id,
+      name,
+      email,
+      password,
+      role
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.message || "회원 정보 수정 중 오류가 발생했습니다.";
+    res.status(status).json({ error: message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await adminService.deleteUser(id);
+    res.status(200).json(result);
+  } catch (error) {
+    const status = error.status || 500;
+    const message = error.message || "회원 삭제 중 오류가 발생했습니다.";
+    res.status(status).json({ error: message });
+  }
+};
+
+module.exports = { createUser, getAllUsers, updateUser, deleteUser };
--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,13 +1,7 @@
 const models = require("../models");
 const bcrypt = require("bcrypt");
 
-const createUser = async (email, name, password, role, userId) => {
-  const loginedUser = await models.User.findByPk(userId);
-  if (loginedUser.role !== "admin") {
-    const error = new Error("관리자만 접근 가능합니다.");
-    error.status = 403;
-    throw error;
-  }
+const createUser = async (email, name, password, role) => {
   const hashedPw = await bcrypt.hash(password, 10);
   const user = await models.User.create({
     email,
@@ -22,10 +16,46 @@ const createUser = async (email, name, password, role, userId) => {
   };
 };
 
-const viewUsersList = async (params) => {};
+const getAllUsers = async () => {
+  const users = await models.User.findAll({
+    attributes: ["id", "email", "name", "role", "createdAt"],
+    order: [["createdAt", "DESC"]],
+  });
+  return { users };
+};
 
-const updateUser = async (params) => {};
+const updateUser = async (id, name, email, password, role) => {
+  const user = await models.User.findByPk(id);
+  if (!user) {
+    const error = new Error("회원을 조회할 수 없습니다.");
+    error.status = 404;
+    throw error;
+  }
+  if (name) {
+    user.name = name;
+  }
+  if (email) {
+    user.email = email;
+  }
+  if (password) {
+    user.password = await bcrypt.hash(password, 10);
+  }
+  if (role) {
+    user.role = role;
+  }
+  await user.save();
+  return { message: "회원 정보가 정상적으로 변경되었습니다." };
+};
 
-const deleteUser = async (params) => {};
+const deleteUser = async (id) => {
+  const user = await models.User.findByPk(id);
+  if (!user) {
+    const error = new Error("사용자를 찾을 수 없습니다.");
+    error.status = 404;
+    throw error;
+  }
+  await user.destroy();
+  return { message: `회원(${user.name})을 성공적으로 삭제했습니다.` };
+};
 
-module.exports = { createUser, viewUsersList, updateUser, deleteUser };
+module.exports = { createUser, getAllUsers, updateUser, deleteUser };
